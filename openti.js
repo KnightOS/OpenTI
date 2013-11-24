d9dae70def291b4e551160ca82e4026c13d8c5e8
Drew DeVault <sir@cmpwn.com>
2013-11-24 14:40:01 -0800

    Modify how registers work, instruction decoding

--- openti.js
+++ openti.js
@@ -10,8 +10,31 @@
         self.writeMemory = null;
         self.readPort = null;
         self.writePort = null;
-        var af = bc = de = hl = ix = iy = 0;
-        var pc = 0; sp = 0;
+
+        function uint8(object, name) {
+            Object.defineProperty(object, name, {
+                get: function() {
+                    return this.val;
+                },
+                set: function(val) {
+                    this.val = val;
+                    this.val &= 0xFF;
+                }
+            });
+            return object;
+        };
+        function uint16(object, name) {
+            Object.defineProperty(object, name, {
+                get: function() {
+                    return this.val;
+                },
+                set: function(val) {
+                    this.val = val;
+                    this.val &= 0xFFFF;
+                }
+            });
+            return object;
+        };
         function countSetBits(value) {
             // Used to set the P/V flag under some conditions
             // There are some algorithms that do this faster, but we're already using JavaScript
@@ -34,209 +57,118 @@
         }
         self.registers = (function() {
             var self = this;
-            var af_ = bc_ = de_ = hl_ = ix_ = iy_ = 0;
-            var i = r = 0;
-
-            self.AF = function(value) {
-                if (typeof value !== 'undefined') af = value & 0xFFFF;
-                return af;
+            function reg8pair(name) {
+                var high = name.substr(0, 1);
+                var low = name.substr(1);
+                Object.defineProperty(self, name, {
+                    get: function() {
+                        return this.val;
+                    },
+                    set: function(val) {
+                        this.val = val & 0xFFFF;
+                    }
+                });
+                Object.defineProperty(self, low, {
+                    get: function() {
+                        return this.val & 0xFF;
+                    },
+                    set: function(val) {
+                        self[name] &= ~0xFF;
+                        self[name] |= (val & 0xFF);
+                    }
+                });
+                Object.defineProperty(self, high, {
+                    get: function() {
+                        return this.val >> 8;
+                    },
+                    set: function(val) {
+                        self[name] &= ~0xFF00;
+                        self[name] |= ((val & 0xFF) << 8);
+                    }
+                });
+                self[name] = 0;
             };
-
-            self.BC = function(value) {
-                if (typeof value !== 'undefined') bc = value & 0xFFFF;
-                return bc;
-            };
-
-            self.DE = function(value) {
-                if (typeof value !== 'undefined') de = value & 0xFFFF;
-                return de;
-            };
-
-            self.HL = function(value) {
-                if (typeof value !== 'undefined') hl = value & 0xFFFF;
-                return hl;
-            };
-
-            self.IX = function(value) {
-                if (typeof value !== 'undefined') ix = value & 0xFFFF;
-                return ix;
-            };
-
-            self.IY = function(value) {
-                if (typeof value !== 'undefined') iy = value & 0xFFFF;
-                return iy;
-            };
-
-            self.PC = function(value) {
-                if (typeof value !== 'undefined') pc = value & 0xFFFF;
-                return pc;
-            };
-            
-            self.SP = function(value) {
-                if (typeof value !== 'undefined') sp = value & 0xFFFF;
-                return sp;
-            };
-
-            self.I = function(value) {
-                if (typeof value !== 'undefined') self.i = value & 0xFF;
-                return i;
-            };
-
-            self.R = function(value) {
-                if (typeof value !== 'undefined') self.r = value & 0xFF;
-                return r;
+            function reg16(name) {
+                Object.defineProperty(self, name, {
+                    get: function() {
+                        return this.val;
+                    },
+                    set: function(val) {
+                        this.val = val & 0xFFFF;
+                    }
+                });
+                self[name] = 0;
             }
-
-            self.A = function(value) {
-                if (typeof value !== 'undefined') {
-                    af &= 0x00FF;
-                    af |= (value & 0xFF) << 8;
-                }
-                return af >> 8;
-            };
-
-            self.F = function(value) {
-                if (typeof value !== 'undefined') {
-                    af &= 0xFF00;
-                    af |= value & 0xFF;
-                }
-                return af & 0x00FF;
-            };
-
-            self.B = function(value) {
-                if (typeof value !== 'undefined') {
-                    bc &= 0x00FF;
-                    bc |= (value & 0xFF) << 8;
-                }
-                return bc >> 8;
-            };
-
-            self.C = function(value) {
-                if (typeof value !== 'undefined') {
-                    bc &= 0xFF00;
-                    bc |= value & 0xFF;
-                }
-                return bc & 0x00FF;
-            };
-
-            self.D = function(value) {
-                if (typeof value !== 'undefined') {
-                    de &= 0x00FF;
-                    de |= (value & 0xFF) << 8;
-                }
-                return de >> 8;
-            };
-
-            self.E = function(value) {
-                if (typeof value !== 'undefined') {
-                    de &= 0xFF00;
-                    de |= value & 0xFF;
-                }
-                return af & 0x00FF;
-            };
-
-            self.H = function(value) {
-                if (typeof value !== 'undefined') {
-                    hl &= 0x00FF;
-                    hl |= (value & 0xFF) << 8;
-                }
-                return hl >> 8;
-            };
-
-            self.L = function(value) {
-                if (typeof value !== 'undefined') {
-                    hl &= 0xFF00;
-                    hl |= value & 0xFF;
-                }
-                return hl & 0x00FF;
-            };
-            
             // TODO: IXH/IXL/IYH/IYL
+            reg8pair('AF');
+            reg8pair('BC');
+            reg8pair('DE');
+            reg8pair('HL');
+            reg16('PC');
+            reg16('SP');
+            reg16('IX');
+            reg16('IY');
+            // Shadow regs
+            var _af = _bc = _de = _hl = 0;
             
-            self.flags = {
-                S: function(value) {
-                    if (typeof value !== 'undefined') {
-                        if (value) af |=  (1 << 7);
-                        else       af &= ~(1 << 7);
+            self.flags = {};
+            function flagdef(name, bit) {
+                Object.defineProperty(self.flags, name, {
+                    get: function() {
+                        return (self.AF & (1 << bit)) > 0;
+                    },
+                    set: function(val) {
+                        if (val) self.AF |=  (1 << bit);
+                        else     self.AF &= ~(1 << bit);
                     }
-                    return (af & (1 << 7)) > 0;
-                },
-                Z: function(value) {
-                    if (typeof value !== 'undefined') {
-                        if (value) af |=  (1 << 6);
-                        else       af &= ~(1 << 6);
+                });
+            };
+            flagdef('S', 7);
+            flagdef('Z', 6);
+            flagdef('H', 4);
+            flagdef('PV', 2);
+            flagdef('N', 1);
+            flagdef('C', 0);
+            self.updateFlags = function(oldValue, newValue, subtraction, unaffected, parity) {
+                if (!unaffected) unaffected = '';
+                if (unaffected.indexOf('S') == -1)
+                    self.flags.S = (self.A() & 0x80) == 0x80;
+                if (unaffected.indexOf('Z') == -1)
+                    self.flags.Z = newValue == 0;
+                // TODO: Half carry
+                if (unaffected.indexOf('P') == -1) {
+                    if (parity) {
+                        self.flags.PV = countSetBits(self.A()) % 2 == 0;
+                    } else {
+                        self.flags.PV = (oldValue & 0x80) == (newValue & 0x80);
                     }
-                    return (af & (1 << 6)) > 0;
-                },
-                H: function(value) {
-                    if (typeof value !== 'undefined') {
-                        if (value) af |=  (1 << 4);
-                        else       af &= ~(1 << 4);
-                    }
-                    return (af & (1 << 4)) > 0;
-                },
-                PV: function(value) {
-                    if (typeof value !== 'undefined') {
-                        if (value) af |=  (1 << 2);
-                        else       af &= ~(1 << 2);
-                    }
-                    return (af & (1 << 2)) > 0;
-                },
-                N: function(value) {
-                    if (typeof value !== 'undefined') {
-                        if (value) af |=  (1 << 1);
-                        else       af &= ~(1 << 1);
-                    }
-                    return (af & (1 << 1)) > 0;
-                },
-                C: function(value) {
-                    if (typeof value !== 'undefined') {
-                        if (value) af |=  (1 << 0);
-                        else       af &= ~(1 << 0);
-                    }
-                    return (af & (1 << 0)) > 0;
-                },
-                update: function(oldValue, newValue, subtraction, unaffected, parity) {
-                    if (!unaffected) unaffected = '';
-                    if (unaffected.indexOf('S') == -1)
-                        self.flags.S((self.A() & 0x80) == 0x80);
-                    if (unaffected.indexOf('Z') == -1)
-                        self.flags.Z(newValue == 0);
-                    // TODO: Half carry
-                    if (unaffected.indexOf('P') == -1) {
-                        if (parity) {
-                            self.flags.PV(countSetBits(self.A()) % 2 == 0);
-                        } else {
-                            self.flags.PV((oldValue & 0x80) == (newValue & 0x80));
-                        }
-                    }
-                    if (unaffected.indexOf('N') == -1)
-                        self.flags.N(subtraction);
-                    if (unaffected.indexOf('C') == -1) {
-                        if (subtraction) {
-                            self.flags.C(newValue > oldValue);
-                        } else {
-                            self.flags.C(newValue < oldValue);
-                        }
+                }
+                if (unaffected.indexOf('N') == -1)
+                    self.flags.N = subtraction;
+                if (unaffected.indexOf('C') == -1) {
+                    if (subtraction) {
+                        self.flags.C = newValue > oldValue;
+                    } else {
+                        self.flags.C = newValue < oldValue;
                     }
                 }
             };
 
             self.exx = function() {
                 var temp;
-                temp = bc; bc = self.bc_; self.bc_ = temp;
-                temp = hl; hl = self.hl_; self.hl_ = temp;
-                temp = de; de = self.de_; self.de_ = temp;
+                temp = self.BC; self.BC = bc_; bc_ = temp;
+                temp = self.HL; self.HL = hl_; hl_ = temp;
+                temp = self.DE; self.DE = de_; de_ = temp;
             };
 
             self.exAF = function() {
                 var temp;
-                temp = af; af = self.af_; self.af_ = temp;
+                temp = self.AF; self.AF = af_; af_ = temp;
             };
 
             self.exDEHL = function() {
                 var temp;
-                temp = de; de = hl; hl = temp;
+                temp = self.DE; self.DE = self.HL; self.HL = temp;
             };
 
             return self;
@@ -252,110 +184,23 @@
 
         self.execute = function(cycles) {
             while (cycles > 0) {
-                var instruction = self.readMemory(pc++);
-                var newValue, bit;
-                switch (instruction) {
-                    case 0x00: // nop
-                        cycles -= 4;
-                        break;
-                    case 0x01: // ld bc, imm16
-                        bc = readWord(pc);
-                        pc += 2;
-                        cycles -= 10;
-                        break;
-                    case 0x02: // ld (bc), a
-                        self.writeMemory(bc, af >> 8);
-                        cycles -= 7;
-                        break;
-                    case 0x03: // inc bc
-                        bc++;
-                        cycles -= 6;
-                        break;
-                    case 0x04: // inc b
-                        newValue = (self.registers.B() + 1) & 0xFF;
-                        self.registers.flags.update(self.registers.B(), newValue, false, 'C');
-                        self.registers.B(newValue);
-                        cycles -= 4;
-                        break;
-                    case 0x05: // dec b
-                        newValue = (self.registers.B() - 1) & 0xFF;
-                        self.registers.flags.update(self.registers.B(), newValue, true, 'C');
-                        self.registers.B(newValue);
-                        cycles -= 4;
-                        break;
-                    case 0x06: // ld b, imm8
-                        bc &= 0x00FF;
-                        bc |= self.readMemory(pc++) << 8;
-                        cycles -= 7;
-                        break;
-                    case 0x07: // rlca
-                        bit = (af & 0x8000) > 0;
-                        newValue = ((af << 1) & 0xFF00);
-                        af &= 0x00FF; af |= newValue;
-                        self.registers.flags.C(bit);
-                        self.registers.flags.N(false);
-                        self.registers.flags.H(false);
-                        if (bit) af |= 0x0100;
-                        cycles -= 4;
-                        break;
-                    case 0x08: // ex af, af'
-                        self.registers.exAF();
-                        cycles -= 4;
-                        break;
-                    case 0x09: // add hl, bc
-                        newValue = (hl + bc) & 0xFFFF;
-                        self.registers.flags.update(hl, newValue, false, 'ZSP');
-                        hl = newValue;
-                        cycles -= 11;
-                        break;
-                    case 0x0A: // ld a, (bc)
-                        self.registers.A(self.readMemory(bc));
-                        cycles -= 11;
-                        break;
-                    case 0x0B: // dec bc
-                        bc = (bc - 1) & 0xFFFF;
-                        cycles -= 6;
-                        break;
-                    case 0x0C: // inc c
-                        newValue = (self.registers.C() + 1) & 0xFF;
-                        self.flags.update(self.registers.C(), newValue, false, 'C');
-                        self.registers.C(newValue);
-                        cycles -= 4;
-                        break;
-                    case 0x0D: // dec c
-                        newValue = (self.registers.C() - 1) & 0xFF;
-                        self.flags.update(self.registers.C(), newValue, true, 'C');
-                        self.registers.C(newValue);
-                        cycles -= 4;
-                        break;
-                    case 0x0E: // ld c, imm8
-                        bc &= 0xFF00;
-                        bc |= self.readMemory(pc++);
-                        cycles -= 7;
-                        break;
-                    case 0x0F: // rrca
-                        bit = (af & 0x0100) > 0;
-                        newValue = ((af >> 1) & 0xFF00);
-                        af &= 0x00FF; af |= newValue;
-                        self.registers.flags.C(bit);
-                        self.registers.flags.N(false);
-                        self.registers.flags.H(false);
-                        if (bit) af |= 0x8000;
-                        cycles -= 4;
-                        break;
-                    case 0x10: // djnz imm8
-                        newValue = ((bc & 0xFF00) >> 8) - 1;
-                        newValue &= 0xFF;
-                        bc |= newValue;
-                        if (newValue != 0) {
-                            pc += readSigned(pc);
-                            cycles -= 8;
-                        } else cycles -= 13;
-                        pc++;
-                        break;
-                    default:
-                        cycles--; // TODO: Raise some sort of error
-                        break;
+                var r = self.registers;
+                var instruction = self.readMemory(r.PC++);
+                if (instruction == 0xCB || instruction == 0xDD || instruction == 0xED || instruction == 0xFD) { // Prefix bytes
+                    // TODO
+                    var opcode = self.readMemory(r.PC++);
+                    cycles--;
+                } else {
+                    var opcode = instruction;
+                    // Decode
+                    var x = (opcode & 0xC0) >> 6;
+                    var y = (opcode & 0x38) >> 3;
+                    var z = (opcode & 0x07);
+                    var p = (opcode & 0x30) >> 4;
+                    var q = (opcode & 0x08) >> 3;
+                    function d() { return self.readMemory(r.PC++); }
+                    function n() { return self.readMemory(r.PC++); }
+                    function nn() { var r = readWord(r.PC); r.PC += 2; return r; }
                 }
             }
             return cycles;
