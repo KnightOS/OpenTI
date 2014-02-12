3d0301d07bd7416d6d9f63281be418f7531f3aa1
Drew DeVault <sir@cmpwn.com>
2014-02-12 14:06:55 -0800

    Update tests

--- tests.js
+++ tests.js
@@ -1,26 +1,40 @@
 var OpenTI = require('./openti.js');
 
+/*
+ * To add a test, just add it to the tests object. The test context
+ * works like this:
+
+test.stage([ memory contents ], { initial register values }, { initial flag values });
+test.execute(cycles);
+test.assert({ expected register values }, { expected flag values }, { expected cycles });
+
+ * The only required parameter in all of these is the memory contents of test.stage.
+ * Everything else uses sane defaults, or omits that value from the test.
+ *
+ * Please make an effort to test all relevant registers, flags, and expected cycles.
+ */
+
 var tests = {
     'ADD A, n': function(test) {
-        // test.stage([ memory contents ], { initial register state }, { initial flag state })
-        test.stage([ 0x80 ], { A: 10, B: 20 });
+        test.stage([ 0x80 /* ADD A, B */ ], { A: 10, B: 20 });
         test.execute();
-        // test.assert({ expected register values }, { expected flag values })
-        test.assert({ A: 30, B: 20 }, { Z: 0, C: 0 });
+        test.assert({ A: 30, B: 20 }, { Z: 0, C: 0 }, 4);
 
-        test.stage([ 0x80 ], { A: 0xF0, B: 0x20 });
+        test.stage([ 0x80 /* ADD A, B */ ], { A: 0xF0, B: 0x20 });
         test.execute();
-        test.assert({ A: 0x10, B: 0x20 }, { Z: 0, C: 1 });
+        test.assert({ A: 0x10, B: 0x20 }, { Z: 0, C: 1 }, 4);
 
-        test.stage([ 0x80 ], { A: 0xF0, B: 0x10 });
+        test.stage([ 0x86 /* ADD A, (HL) */ ], { A: 0, HL: 0xC000 });
+        test.calc.mmu.forceWrite(0xC000, 10);
         test.execute();
-        test.assert({ A: 0, B: 0x10 }, { Z: 1, C: 1 });
+        test.assert({ A: 10 }, { Z: 0, C: 0 }, 7);
     }
 };
 
 function createTestContext() {
     return {
         calc: new OpenTI.TI83p(),
+        cycles: 0,
         stage: function(code, registers, flags) {
             this.reset();
             for (var i = 0; i < code.length; i++) {
@@ -37,7 +51,7 @@ function createTestContext() {
                 }
             }
         },
-        assert: function(registers, flags) {
+        assert: function(registers, flags, cycles) {
             for (var r in registers) {
                 if (registers[r] != this.calc.cpu.registers[r]) {
                     throw new Error('Expected register ' + r + ' to be ' + registers[r] + ', was actually ' + this.calc.cpu.registers[r]);
@@ -50,12 +64,20 @@ function createTestContext() {
                     }
                 }
             }
+            if (typeof cycles !== 'undefined') {
+                if (cycles != this.cycles) {
+                    throw new Error('Expected cycles to be ' + cycles + ', was actually ' + this.cycles);
+                }
+            }
         },
         reset: function() {
             this.calc = new OpenTI.TI83p();
+            this.cycles = 0;
         },
-        execute: function() {
-            this.calc.cpu.execute(1);
+        execute: function(cycles) {
+            var result = this.calc.cpu.execute(typeof cycles === 'undefined' ? 1 : cycles);
+            result -= typeof cycles === 'undefined' ? 1 : cycles;
+            this.cycles = -result;
         }
     };
 }
