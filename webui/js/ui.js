d1aa8791e4c2290e50122de883560ded19abc89a
Nathaniel van Diepen <eeems@omnimaga.org>
2014-08-05 14:52:49 -0600

    Updated scope, fixed style

--- webui/js/ui.js
+++ webui/js/ui.js
@@ -1,19 +1,19 @@
-$(function(){
-	$(".panel").each(function(a){
-		var span = $('<span>').addClass('glyphicon pull-right');
-		if($(a).hasClass("collapsed")){
-			span.addClass('glyphicon-chevron-down');
-		}else{
-			span.addClass('glyphicon-chevron-up');
-		}
-		$(".panel-title",a).append(span);
-		$(".panel-heading",a).click(function(){
-			$("span",this)
-				.toggleClass("glyphicon-chevron-up")
-				.toggleClass("glyphicon-chevron-down");
-			$(this)
-				.parent()
-				.toggleClass("collapsed");
-		});
-	});
+$(function() {
+    $(".panel").each(function(a) {
+        var span = $('<span>').addClass('glyphicon pull-right');
+        if ($(a).hasClass("collapsed")) {
+            span.addClass('glyphicon-chevron-down');
+        }else{
+            span.addClass('glyphicon-chevron-up');
+        }
+        $(".panel-title",a).append(span);
+        $(".panel-heading",a).click(function() {
+            $("span",this)
+                .toggleClass("glyphicon-chevron-up")
+                .toggleClass("glyphicon-chevron-down");
+            $(this)
+                .parent()
+                .toggleClass("collapsed");
+        });
+    });
 });
